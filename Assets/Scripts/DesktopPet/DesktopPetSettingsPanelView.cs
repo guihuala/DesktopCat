a16422a206a03ba3f3b98e1d735471c3fba1a4d7
桂糕 <2509664638@qq.com>
2026-06-10 18:25:03 +0800

    feat：基础桌宠功能测试

---
+++ Assets/Scripts/DesktopPet/DesktopPetSettingsPanelView.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DesktopPet
+{
+    public class DesktopPetSettingsPanelView : MonoBehaviour
+    {
+        [SerializeField] private Slider scaleSlider;
+        [SerializeField] private Text scaleValueText;
+        [SerializeField] private Button closeButton;
+        [SerializeField] private Button resetButton;
+
+        private Transform petRoot;
+        private float defaultScale;
+
+        public void Initialize(Transform targetPetRoot, float minScale, float maxScale, float initialScale, float resetScale)
+        {
+            petRoot = targetPetRoot;
+            defaultScale = resetScale;
+
+            if (scaleSlider != null)
+            {
+                scaleSlider.minValue = minScale;
+                scaleSlider.maxValue = maxScale;
+                scaleSlider.wholeNumbers = false;
+                scaleSlider.onValueChanged.RemoveListener(SetPetScale);
+                scaleSlider.onValueChanged.AddListener(SetPetScale);
+                scaleSlider.SetValueWithoutNotify(initialScale);
+            }
+
+            if (closeButton != null)
+            {
+                closeButton.onClick.RemoveListener(Close);
+                closeButton.onClick.AddListener(Close);
+            }
+
+            if (resetButton != null)
+            {
+                resetButton.onClick.RemoveListener(ResetScale);
+                resetButton.onClick.AddListener(ResetScale);
+            }
+
+            SetPetScale(initialScale);
+        }
+
+        public void Open()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Toggle()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+
+        private void ResetScale()
+        {
+            if (scaleSlider != null)
+            {
+                scaleSlider.value = defaultScale;
+            }
+            else
+            {
+                SetPetScale(defaultScale);
+            }
+        }
+
+        private void SetPetScale(float value)
+        {
+            if (petRoot != null)
+            {
+                petRoot.localScale = Vector3.one * value;
+            }
+
+            if (scaleValueText != null)
+            {
+                scaleValueText.text = $"{value:0.00}x";
+            }
+        }
+    }
+}
